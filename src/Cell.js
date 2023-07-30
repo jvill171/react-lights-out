7c46f4e726759ef35a89283a211a3480bbcc233f
jvill171 <41810008+jvill171@users.noreply.github.com>
2023-07-29 20:56:20 -0500

    Moved a single space after the addition of the Cell class to be conditionally there as the Cell-lit class is added

--- src/Cell.js
+++ src/Cell.js
@@ -15,7 +15,7 @@ import "./Cell.css";
  **/
 
 function Cell({ flipCellsAroundMe, isLit }) {
-  const classes = `Cell ${isLit ? "Cell-lit" : ""}`;
+  const classes = `Cell${isLit ? " Cell-lit" : ""}`;
   return <td className={classes} onClick={flipCellsAroundMe} />;
 }
 
